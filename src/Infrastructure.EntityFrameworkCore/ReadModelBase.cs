ef069fc5eacfdaf7a906a9c999238f800169c5e5
afandylamusu <afandy.lamusu@live.co.uk>
2018-12-13 23:14:19 +0700

    Update

--- src/Infrastructure.EntityFrameworkCore/ReadModelBase.cs
+++ src/Infrastructure.EntityFrameworkCore/ReadModelBase.cs
@@ -33,12 +33,4 @@ namespace Infrastructure.Domain.ReadModels
         public string DeletedBy { get; set; }
         #endregion
     }
-
-    //public static class ReadModelBaseExt
-    //{
-    //    public static void AddDomainEvent<T, TProperty>(this T model, Expression<Func<T, TProperty>> expression) where T : ReadModelBase
-    //    {
-    //        model.AddDomainEvent(new OnEntityUpdated<T, TProperty>(expression));
-    //    }
-    //}
 }
