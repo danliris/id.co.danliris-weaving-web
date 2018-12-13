e783a348427942a90a696fca40ea4e72cb0df228
afandylamusu <afandy.lamusu@live.co.uk>
2018-12-13 23:12:29 +0700

    Update

--- src/Barebone/ExtensionMetadata.cs
+++ src/Barebone/ExtensionMetadata.cs
@@ -14,8 +14,8 @@ namespace Barebone
             {
                 return new StyleSheet[]
                 {
-          new StyleSheet("//fonts.googleapis.com/css?family=Open+Sans", 100),
-          new StyleSheet("/Styles.barebone.css", 200)
+                    new StyleSheet("//fonts.googleapis.com/css?family=Open+Sans", 100),
+                    new StyleSheet("/Styles.barebone.css", 200)
                 };
             }
         }
@@ -26,10 +26,10 @@ namespace Barebone
             {
                 return new Script[]
                 {
-          new Script("//ajax.aspnetcdn.com/ajax/jquery/jquery-1.11.3.min.js", 100),
-          new Script("//ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.min.js", 200),
-          new Script("//ajax.aspnetcdn.com/ajax/jquery.validation.unobtrusive/3.2.6/jquery.validate.unobtrusive.min.js", 300),
-          new Script("/Scripts.barebone.js", 400)
+                    new Script("//ajax.aspnetcdn.com/ajax/jquery/jquery-1.11.3.min.js", 100),
+                    new Script("//ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.min.js", 200),
+                    new Script("//ajax.aspnetcdn.com/ajax/jquery.validation.unobtrusive/3.2.6/jquery.validate.unobtrusive.min.js", 300),
+                    new Script("/Scripts.barebone.js", 400)
                 };
             }
         }
