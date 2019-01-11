fbec3c2eebfc0ec062ca2e2377547eba0c53cb8e
AchmadSetiaji <achmad.setiaji@moonlay.com>
2019-01-11 19:00:52 +0700

    update-migration-file

---
+++ src/WebApplication/Migrations/20190111115911_Initial-Weaving.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DanLiris.Admin.Web.Migrations
+{
+    public partial class InitialWeaving : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WeavingOrderDocuments",
+                columns: table => new
+                {
+                    Identity = table.Column<Guid>(nullable: false),
+                    RowVersion = table.Column<byte[]>(rowVersion: true, nullable: true),
+                    CreatedBy = table.Column<string>(maxLength: 32, nullable: false),
+                    CreatedDate = table.Column<DateTimeOffset>(nullable: false),
+                    ModifiedBy = table.Column<string>(maxLength: 32, nullable: true),
+                    ModifiedDate = table.Column<DateTimeOffset>(nullable: true),
+                    Deleted = table.Column<bool>(nullable: true),
+                    DeletedDate = table.Column<DateTimeOffset>(nullable: true),
+                    DeletedBy = table.Column<string>(maxLength: 32, nullable: true),
+                    OrderNumber = table.Column<string>(nullable: true),
+                    DateOrdered = table.Column<DateTimeOffset>(nullable: false),
+                    WarpOrigin = table.Column<string>(nullable: true),
+                    WeftOrigin = table.Column<string>(nullable: true),
+                    WholeGrade = table.Column<int>(nullable: false),
+                    YarnType = table.Column<string>(nullable: true),
+                    Period = table.Column<string>(maxLength: 255, nullable: true),
+                    Composition = table.Column<string>(maxLength: 255, nullable: true),
+                    FabricConstructionDocument = table.Column<string>(maxLength: 255, nullable: true),
+                    WeavingUnit = table.Column<string>(maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeavingOrderDocuments", x => x.Identity);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeavingOrderDocuments");
+        }
+    }
+}
