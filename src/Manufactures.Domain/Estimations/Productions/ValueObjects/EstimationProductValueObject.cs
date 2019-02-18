49c0d8c3fa697bb0bd23151137f6cfe4136909ee
AchmadSetiaji <achmad.setiaji@moonlay.com>
2019-02-18 13:05:38 +0700

    update for fix on add estimationProducts

--- src/Manufactures.Domain/Estimations/Productions/ValueObjects/EstimationProductValueObject.cs
+++ src/Manufactures.Domain/Estimations/Productions/ValueObjects/EstimationProductValueObject.cs
@@ -8,22 +8,45 @@ namespace Manufactures.Domain.Estimations.Productions.ValueObjects
     public class EstimationProductValueObject : ValueObject
     {
         // From Construction Document
-        public string ConstructionNumber { get; set; }
-        public double AmountTotal { get; set; }
+        public string ConstructionNumber { get; private set; }
+        public double TotalYarn { get; private set; }
 
         // From SOP
-        public DateTimeOffset DateOrdered { get; set; }
-        public string OrderNumber { get; set; }
-        public int OrderTotal { get; set; }
+        public DateTimeOffset DateOrdered { get; private set; }
+        public string OrderNumber { get; private set; }
+        public int WholeGrade { get; private set; }
 
         // Calculation from totalyarn * TotalOrder From SOP
-        public double TotalGramEstimation { get; set; }
+        public double TotalGramEstimation { get; private set; }
 
         // Product Grade
-        public int GradeA { get; set; }
-        public int GradeB { get; set; }
-        public int GradeC { get; set; }
-        public int GradeD { get; set; }
+        public int GradeA { get; private set; }
+        public int GradeB { get; private set; }
+        public int GradeC { get; private set; }
+        public int GradeD { get; private set; }
+        
+        public EstimationProductValueObject(string constructionNumber, 
+                                            double totalYarn,
+                                            DateTimeOffset dateOrdered,
+                                            string orderNumber,
+                                            int wholeGrade,
+                                            double totalGramEstimation,
+                                            int gradeA,
+                                            int gradeB,
+                                            int gradeC,
+                                            int gradeD)
+        {
+            ConstructionNumber = constructionNumber;
+            TotalYarn = totalYarn;
+            DateOrdered = dateOrdered;
+            OrderNumber = orderNumber;
+            WholeGrade = wholeGrade;
+            TotalGramEstimation = totalGramEstimation;
+            GradeA = gradeA;
+            GradeB = gradeB;
+            GradeC = gradeC;
+            GradeD = gradeD;
+        }
 
         protected override IEnumerable<object> GetAtomicValues()
         {
@@ -31,8 +54,8 @@ namespace Manufactures.Domain.Estimations.Productions.ValueObjects
             yield return OrderNumber;
             yield return ConstructionNumber;
             yield return TotalGramEstimation;
-            yield return AmountTotal;
-            yield return OrderTotal;
+            yield return TotalYarn;
+            yield return WholeGrade;
             yield return GradeA;
             yield return GradeB;
             yield return GradeC;
