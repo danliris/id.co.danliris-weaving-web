4c36ec6c78473daac1becd42915a9dae385f0cdd
AchmadSetiaji <achmad.setiaji@moonlay.com>
2019-02-19 15:58:36 +0700

    update for add estimation number

--- src/Manufactures.Data.EntityFrameworkCore.SqlServer/Estimations/Productions/Repositories/EstimationProductionRepository.cs
+++ src/Manufactures.Data.EntityFrameworkCore.SqlServer/Estimations/Productions/Repositories/EstimationProductionRepository.cs
@@ -2,6 +2,10 @@
 using Manufactures.Domain.Estimations.Productions;
 using Manufactures.Domain.Estimations.Productions.ReadModels;
 using Manufactures.Domain.Estimations.Productions.Repositories;
+using Manufactures.Domain.GlobalValueObjects;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Manufactures.Data.EntityFrameworkCore.Estimations.Productions.Repositories
 {
@@ -10,6 +14,17 @@ namespace Manufactures.Data.EntityFrameworkCore.Estimations.Productions.Reposito
         protected override EstimatedProductionDocument Map(EstimatedProductionDocumentReadModel readModel)
         {
             return new EstimatedProductionDocument(readModel);
+        }
+
+        public async Task<string> GetEstimationNumber()
+        {
+            DateTimeOffset now = DateTimeOffset.Now;
+            var year = now.Year.ToString();
+            var month = now.Month.ToString();
+            var estimationNumber = (this.dbSet.Where(o => o.Period.Deserialize<Period>().Year.Contains(year))).Count() + 1.ToString();
+            estimationNumber = estimationNumber.PadLeft(4, '0') + "/" + month.PadLeft(2, '0') + "-" + year;
+
+            return await Task.FromResult(estimationNumber);
         }
     }
 }
