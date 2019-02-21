54bc8786642bf1937b978a36e388beb13514b7ce
AchmadSetiaji <achmad.setiaji@moonlay.com>
2019-02-21 19:09:15 +0700

    update dto for detail

--- src/Manufactures/Dtos/MaterialTypeDto.cs
+++ src/Manufactures/Dtos/MaterialTypeDto.cs
@@ -1,21 +1,28 @@
-﻿using Manufactures.Domain.Materials;
+﻿using Manufactures.Domain.GlobalValueObjects;
+using Manufactures.Domain.Materials;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Manufactures.Dtos
 {
     public class MaterialTypeDto
     {
+
+        public Guid Id { get; private set; }
+        public string Code { get; private set; }
+        public string Name { get; private set; }
+        public List<RingDocumentValueObject> RingDocuments { get; private set; }
+        public string Description { get; private set; }
+
         public MaterialTypeDto(MaterialTypeDocument materialType)
         {
             Id = materialType.Identity;
             Code = materialType.Code;
             Name = materialType.Name;
+            RingDocuments = materialType.RingDocuments.ToList();
             Description = materialType.Description;
          }
 
-        public Guid Id { get; private set; }
-        public string Code { get; private set; }
-        public string Name { get; private set; }
-        public string Description { get; private set; }
     }
 }
--- src/Manufactures/Dtos/RingDocumentDto.cs
+++ src/Manufactures/Dtos/RingDocumentDto.cs
@@ -5,17 +5,19 @@ namespace Manufactures.Dtos
 {
     public class RingDocumentDto
     {
+        public Guid Id { get; private set; }
+        public string Code { get; private set; }
+        public int Number { get; private set; }
+        public string RingType { get; private set; }
+        public string Description { get; private set; }
+
         public RingDocumentDto(RingDocument ringDocument)
         {
             Id = ringDocument.Identity;
             Code = ringDocument.Code;
             Number = ringDocument.Number;
+            RingType = ringDocument.RingType;
             Description = ringDocument.Description;
         }
-
-        public Guid Id { get; private set; }
-        public string Code { get; private set; }
-        public int Number { get; private set; }
-        public string Description { get; private set; }
     }
 }
