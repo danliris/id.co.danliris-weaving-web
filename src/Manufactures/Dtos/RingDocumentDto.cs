230bfca9f4a8935ac3f96b1ad3bbbec09035a21d
AchmadSetiaji <achmad.setiaji@moonlay.com>
2019-02-25 18:00:43 +0700

    update dto to make sure not changed (test)

--- src/Manufactures/Dtos/RingDocumentDto.cs
+++ src/Manufactures/Dtos/RingDocumentDto.cs
@@ -1,14 +1,24 @@
 ﻿using Manufactures.Domain.Rings;
+using Newtonsoft.Json;
 using System;
 
 namespace Manufactures.Dtos
 {
     public class RingDocumentDto
     {
+        [JsonProperty(PropertyName = "Id")]
         public Guid Id { get; private set; }
+
+        [JsonProperty(PropertyName = "Code")]
         public string Code { get; private set; }
+
+        [JsonProperty(PropertyName = "Number")]
         public int Number { get; private set; }
+
+        [JsonProperty(PropertyName = "RingType")]
         public string RingType { get; private set; }
+
+        [JsonProperty(PropertyName = "Description")]
         public string Description { get; private set; }
 
         public RingDocumentDto(RingDocument ringDocument)
