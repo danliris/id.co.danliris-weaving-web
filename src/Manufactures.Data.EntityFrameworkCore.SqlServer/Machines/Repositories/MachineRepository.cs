9af081cf0fd6b4c07e608885fd5d358cdfecd90d
AchmadSetiaji <achmad.setiaji@moonlay.com>
2019-03-19 18:28:05 +0700

    add repository for machineDocument

---
+++ src/Manufactures.Data.EntityFrameworkCore.SqlServer/Machines/Repositories/MachineRepository.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Data.EntityFrameworkCore;
+using Manufactures.Domain.Machines;
+using Manufactures.Domain.Machines.ReadModels;
+using Manufactures.Domain.Machines.Repositories;
+
+namespace Manufactures.Data.EntityFrameworkCore.Machines.Repositories
+{
+    public class MachineRepository : AggregateRepostory<MachineDocument, MachineDocumentReadModel>, IMachineRepository
+    {
+        protected override MachineDocument Map(MachineDocumentReadModel readModel)
+        {
+            return new MachineDocument(readModel);
+        }
+    }
+}
