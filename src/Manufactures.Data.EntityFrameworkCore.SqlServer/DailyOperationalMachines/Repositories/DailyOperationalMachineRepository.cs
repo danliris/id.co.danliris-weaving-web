4587b07d8b2904ab8ef7052a211b3c024ae2bc9a
Muhammad MFP. Fakhrian Putra <muhammad.putra@moonlay.jakarta>
2019-04-04 18:12:22 +0700

    Weaving API Add Repository on Domain and on Infrastructure

---
+++ src/Manufactures.Data.EntityFrameworkCore.SqlServer/DailyOperationalMachines/Repositories/DailyOperationalMachineRepository.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Data.EntityFrameworkCore;
+using Manufactures.Domain.DailyOperations;
+using Manufactures.Domain.DailyOperations.ReadModels;
+using Manufactures.Domain.DailyOperations.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manufactures.Data.EntityFrameworkCore.DailyOperationalMachines.Repositories
+{
+    public class DailyOperationalMachineRepository : AggregateRepostory<DailyOperationMachineDocument, DailyOperationMachineDocumentReadModel>, IDailyOperationalMachineRepository
+    {
+        protected override DailyOperationMachineDocument Map(DailyOperationMachineDocumentReadModel readModel)
+        {
+            return new DailyOperationMachineDocument(readModel);
+        }
+    }
+}
---
+++ src/Manufactures.Domain/DailyOperations/Repositories/IDailyOperationalMachineRepository.cs
@@ -0,0 +1,12 @@
+﻿using Infrastructure.Domain.Repositories;
+using Manufactures.Domain.DailyOperations.ReadModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manufactures.Domain.DailyOperations.Repositories
+{
+    public interface IDailyOperationalMachineRepository : IAggregateRepository<DailyOperationMachineDocument, DailyOperationMachineDocumentReadModel>
+    {
+    }
+}
