90937dfc2a77e9d05d0a7d09e395756174ec0ed3
Lucia Widyana <mo_cia@yahoo.com>
2023-06-20 14:09:17 +0700

    Update WeavingDailyOperationWarpingMachineQueryHandler.cs

--- src/Manufactures.Application/DailyOperations/Warping/QueryHandlers/WeavingDailyOperationWarpingMachineQueryHandler.cs
+++ src/Manufactures.Application/DailyOperations/Warping/QueryHandlers/WeavingDailyOperationWarpingMachineQueryHandler.cs
@@ -42,17 +42,17 @@ namespace Manufactures.Application.DailyOperations.Warping.QueryHandlers
             SqlConnection conn = new SqlConnection(myConnectionString1);
             //try
             //{
-                conn.Open();
-                SqlCommand cmd = new SqlCommand();// Creating instance of SqlCommand  
+            conn.Open();
+            SqlCommand cmd = new SqlCommand();// Creating instance of SqlCommand  
 
-                String sql = "";
-                // set the connection to instance of SqlCommand  
-                sql = @"delete WeavingDailyOperationWarpingMachines where [month]= '" + month + "' and yearperiode='" + year + "'";
-                cmd.CommandText = sql;
-                cmd.Connection = conn;
-                cmd.ExecuteNonQuery();
+            String sql = "";
+            // set the connection to instance of SqlCommand  
+            sql = @"delete WeavingDailyOperationWarpingMachines where [month]= '" + month + "' and yearperiode='" + year + "'";
+            cmd.CommandText = sql;
+            cmd.Connection = conn;
+            cmd.ExecuteNonQuery();
 
-                conn.Close();
+            conn.Close();
 
             //}
             //catch (Exception ex)
@@ -72,20 +72,34 @@ namespace Manufactures.Application.DailyOperations.Warping.QueryHandlers
             string error = "";
             int isSave = 0;
             int saved = 0;
+            int notError = 0;
             foreach (var sheet in sheets)
             {
                 if (!sheet.Name.Trim().Contains("Produksi WP"))
                 {
-                    error = "Tidak terdapat sheet Produksi WP di File Excel";
+                    notError = 1;
                 }
                 else
                 {
-                    error = null;
-                    totalRows = sheet.Dimension.Rows;
-                    var totalColumns = sheet.Dimension.Columns;
+                    notError = 0;
+                    break;
+                }
+            }
 
+
+            if (notError == 1)
+            {
+                error = "Tidak terdapat sheet Produksi WP di File Excel";
+            }
+            else
+            {
+                foreach (var sheet in sheets)
+                {
                     if (sheet.Name.Trim() == "Produksi WP")
                     {
+                        error = null;
+                        totalRows = sheet.Dimension.Rows;
+                        var totalColumns = sheet.Dimension.Columns;
                         try
                         {
                             for (rowIndex = startRow; rowIndex <= totalRows; rowIndex++)
@@ -93,48 +107,43 @@ namespace Manufactures.Application.DailyOperations.Warping.QueryHandlers
 
                                 if (sheet.Cells[rowIndex, startCol].Value != null || sheet.Cells[rowIndex, startCol].Value != "")
                                 {
-                                    if (converter.GenerateValueInt(sheet.Cells[rowIndex, startCol + 7]) == year)
-                                    {
-                                        data = new WeavingDailyOperationWarpingMachine(
-                                      Guid.NewGuid(), //
-                                      Convert.ToInt32(sheet.Cells[rowIndex, startCol].Value), //tgl
-                                      month,
-                                      monthId,//month
-                                      converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 7]),
-                                      year.ToString(),//year
-                                      converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 1]),//shift
-                                      converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 2]),//mcNo
-                                      converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 3]),//Name
-                                      converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 4]),//Group
-                                      converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 5]),//Lot
-                                      converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 6]),//SP
-                                      converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 8]),//YearSP
-                                      converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 9]),//WarpType
-                                      converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 10]),//AL
-                                      converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 11]),//COnstruction
-                                      converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 12]),//Code
-                                      converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 13]),//BeamNo
-                                      Convert.ToInt32(converter.GenerateValueInt(sheet.Cells[rowIndex, startCol + 14])),//TotalCone
-                                      converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 15]),//ThreadNo
-                                      Convert.ToDouble(converter.GenerateValueDouble(sheet.Cells[rowIndex, startCol + 16])),//Length
-                                      "MT",//uom
-                                      Convert.ToDateTime(converter.GeneratePureTime(sheet.Cells[rowIndex, startCol + 17])),//start
-                                      Convert.ToDateTime(converter.GeneratePureTime(sheet.Cells[rowIndex, startCol + 18])),//Doff
-                                      Convert.ToDouble(converter.GenerateValueDouble(sheet.Cells[rowIndex, startCol + 19])),//HNLeft
-                                      Convert.ToDouble(converter.GenerateValueDouble(sheet.Cells[rowIndex, startCol + 20])),//HNMiddle
-                                      Convert.ToDouble(converter.GenerateValueDouble(sheet.Cells[rowIndex, startCol + 21])),//HNRight
-                                      Convert.ToDouble(converter.GenerateValueDouble(sheet.Cells[rowIndex, startCol + 22])),//Speed
-                                      Convert.ToDouble(converter.GenerateValueDouble(sheet.Cells[rowIndex, startCol + 23])),//ThreadCut
-                                      Convert.ToDouble(converter.GenerateValueDouble(sheet.Cells[rowIndex, startCol + 24])),//Capacity
-                                      converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 25])//Eff
-                                      );
-                                        await _repository.Update(data);
-                                        saved = 1;
-                                    }
-                                    else
-                                    {
-                                        isSave = 1;
-                                    }
+                                    //if (converter.GenerateValueInt(sheet.Cells[rowIndex, startCol + 7]) == year)
+                                    //{
+                                    data = new WeavingDailyOperationWarpingMachine(
+                                    Guid.NewGuid(), //
+                                    Convert.ToInt32(sheet.Cells[rowIndex, startCol].Value), //tgl
+                                    month,
+                                    monthId,//month
+                                    converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 7]),
+                                    year.ToString(),//year
+                                    converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 1]),//shift
+                                    converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 2]),//mcNo
+                                    converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 3]),//Name
+                                    converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 4]),//Group
+                                    converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 5]),//Lot
+                                    converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 6]),//SP
+                                    converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 8]),//YearSP
+                                    converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 9]),//WarpType
+                                    converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 10]),//AL
+                                    converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 11]),//COnstruction
+                                    converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 12]),//Code
+                                    converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 13]),//BeamNo
+                                    Convert.ToInt32(converter.GenerateValueInt(sheet.Cells[rowIndex, startCol + 14])),//TotalCone
+                                    converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 15]),//ThreadNo
+                                    Convert.ToDouble(converter.GenerateValueDouble(sheet.Cells[rowIndex, startCol + 16])),//Length
+                                    "MT",//uom
+                                    Convert.ToDateTime(converter.GeneratePureTime(sheet.Cells[rowIndex, startCol + 17])),//start
+                                    Convert.ToDateTime(converter.GeneratePureTime(sheet.Cells[rowIndex, startCol + 18])),//Doff
+                                    Convert.ToDouble(converter.GenerateValueDouble(sheet.Cells[rowIndex, startCol + 19])),//HNLeft
+                                    Convert.ToDouble(converter.GenerateValueDouble(sheet.Cells[rowIndex, startCol + 20])),//HNMiddle
+                                    Convert.ToDouble(converter.GenerateValueDouble(sheet.Cells[rowIndex, startCol + 21])),//HNRight
+                                    Convert.ToDouble(converter.GenerateValueDouble(sheet.Cells[rowIndex, startCol + 22])),//Speed
+                                    Convert.ToDouble(converter.GenerateValueDouble(sheet.Cells[rowIndex, startCol + 23])),//ThreadCut
+                                    Convert.ToDouble(converter.GenerateValueDouble(sheet.Cells[rowIndex, startCol + 24])),//Capacity
+                                    converter.GenerateValueString(sheet.Cells[rowIndex, startCol + 25])//Eff
+                                    );
+                                    await _repository.Update(data);
+                                    saved = 1;
                                 }
                             }
                         }
@@ -144,7 +153,7 @@ namespace Manufactures.Application.DailyOperations.Warping.QueryHandlers
                         }
                     }
                 }
-            
+
             }
             try
             {
@@ -186,7 +195,7 @@ namespace Manufactures.Application.DailyOperations.Warping.QueryHandlers
                                  Group = y.Group,
                                  Name = y.Name,
                                  CreatedDate = y.CreatedDate.ToString("dd-MM-yyyy")
-                             }).OrderByDescending(s => s.CreatedDate);
+                             });
 
             await Task.Yield();
 
@@ -197,7 +206,7 @@ namespace Manufactures.Application.DailyOperations.Warping.QueryHandlers
         {
             var query = _repository
                             .Query.OrderByDescending(o => o.CreatedDate)
-                            .Where(s=>s.Identity == id)
+                            .Where(s => s.Identity == id)
                             .Select(y =>
                              new WeavingDailyOperationWarpingMachineDto
                              {
@@ -208,7 +217,7 @@ namespace Manufactures.Application.DailyOperations.Warping.QueryHandlers
                                  CreatedDate = y.CreatedDate.ToString("dd-MM-yyyy")
                              }).FirstOrDefault();
 
-          
+
 
             return query;
         }
@@ -232,17 +241,17 @@ namespace Manufactures.Application.DailyOperations.Warping.QueryHandlers
                               yearPeriode = a.YearPeriode,
                               day = a.Date,
                               name = a.Name,
-                              Periode = new DateTime(Convert.ToInt32(a.Year), a.MonthId, a.Date)
+                              Periode = new DateTime(Convert.ToInt32(a.YearPeriode), a.MonthId, a.Date)
                           };
             var query = (from a in allData
-                        where 
-                        ((mcNo == null || (mcNo != null && mcNo != "" && a.mcNo == mcNo)) &&
-                        (shift == null || (shift != null && shift != "" && a.shift == shift)) &&
-                        (sp == null || (sp != null && sp != "" && a.sp.Contains(sp))) &&
-                        (threadNo == null || (threadNo != null && threadNo != "" && a.threadNo.Contains(threadNo))) &&
-                        (code == null || (code != null && code != "" && a.code.Contains( code))) &&
-                        (a.Periode.Date  >= fromDate.Date && a.Periode.Date<= toDate.Date))
-                        select new { Name = a.name , ThreadCut  = a.threadCut, Length  =a.length}).GroupBy(l => l.Name)
+                         where
+                         ((mcNo == null || (mcNo != null && mcNo != "" && a.mcNo == mcNo)) &&
+                         (shift == null || (shift != null && shift != "" && a.shift == shift)) &&
+                         (sp == null || (sp != null && sp != "" && a.sp.Contains(sp))) &&
+                         (threadNo == null || (threadNo != null && threadNo != "" && a.threadNo.Contains(threadNo))) &&
+                         (code == null || (code != null && code != "" && a.code.Contains(code))) &&
+                         (a.Periode.Date >= fromDate.Date && a.Periode.Date <= toDate.Date))
+                         select new { Name = a.name, ThreadCut = a.threadCut, Length = a.length }).GroupBy(l => l.Name)
                             .Select(cl => new
                             {
                                 Name = cl.First().Name,
