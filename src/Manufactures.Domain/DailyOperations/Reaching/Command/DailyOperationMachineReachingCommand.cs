0ae9a6d5b6394fbe92ac062d8b1f507fbdff8175
achensetyawati <achenzetya@gmail.com>
2023-06-27 16:40:43 +0700

    daily-operation-reaching

---
+++ src/Manufactures.Domain/DailyOperations/Reaching/Command/DailyOperationMachineReachingCommand.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Infrastructure.Domain.Commands;
+using Manufactures.Domain.DailyOperations.Reaching.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Manufactures.Domain.DailyOperations.Reaching.Command
+{
+    public class DailyOperationMachineReachingCommand : ICommand<DailyOperationMachineReaching>
+    {
+        public Stream stream { get; set; }
+
+    }
+    public class DailyOperationMachineReachingCommandValidator : AbstractValidator<DailyOperationMachineReachingCommand>
+    {
+        public DailyOperationMachineReachingCommandValidator()
+        {
+            var commands = new DailyOperationMachineReachingCommand();
+            RuleFor(command => command.stream).NotEmpty();
+        }
+    }
+}
