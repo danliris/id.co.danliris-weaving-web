f47e6bd20b61c968697317fae162aa3caa4fef7d
achensetyawati <achenzetya@gmail.com>
2023-07-04 11:22:20 +0700

    Reaching

---
+++ src/Manufactures.Domain/DailyOperations/Reaching/Repositories/IDailyOperationReachingMachineRepository.cs
@@ -0,0 +1,13 @@
+﻿using Infrastructure.Domain.Repositories;
+using Manufactures.Domain.DailyOperations.Reaching.Entities;
+using Manufactures.Domain.DailyOperations.Reaching.ReadModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manufactures.Domain.DailyOperations.Reaching.Repositories
+{
+    public interface IDailyOperationReachingMachineRepository : IAggregateRepository<DailyOperationMachineReaching, DailyOperationMachineReachingReadModel>
+    {
+    }
+}
