2efbe4e2ab4b875af01de4268dc7ef6825392f78
achensetyawati <achenzetya@gmail.com>
2023-07-04 11:22:20 +0700

    Reaching

---
+++ src/Manufactures.Data.EntityFrameworkCore.SqlServer/DailyOperations/Reaching/Repositories/DailyOperationMachineReachingRepository.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.Data.EntityFrameworkCore;
+using Manufactures.Domain.DailyOperations.Reaching.Entities;
+using Manufactures.Domain.DailyOperations.Reaching.Queries;
+using Manufactures.Domain.DailyOperations.Reaching.ReadModels;
+using Manufactures.Domain.DailyOperations.Reaching.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manufactures.Data.EntityFrameworkCore.DailyOperations.Reaching.Repositories
+{
+    public class DailyOperationMachineReachingRepository : AggregateRepostory<DailyOperationMachineReaching, DailyOperationMachineReachingReadModel>, IDailyOperationReachingMachineRepository
+    {
+        protected override DailyOperationMachineReaching Map(DailyOperationMachineReachingReadModel readModel)
+        {
+            return new DailyOperationMachineReaching(readModel);
+        }
+    }
+}
