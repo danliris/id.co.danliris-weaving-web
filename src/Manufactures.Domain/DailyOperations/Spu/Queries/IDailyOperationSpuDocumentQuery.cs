9ceaa811afc44477f4005356574005eec79145e6
Andreasaja <under.block22@gmail.com>
2023-07-06 11:11:50 +0700

    spu baca warping

---
+++ src/Manufactures.Domain/DailyOperations/Spu/Queries/IDailyOperationSpuDocumentQuery.cs
@@ -0,0 +1,8 @@
+﻿using Infrastructure.Domain.Queries;
+
+namespace Manufactures.Domain.DailyOperations.Spu.Queries
+{
+    public interface IDailyOperationSpuDocumentQuery<TModel> : IQueries<TModel>
+    {
+    }
+}
