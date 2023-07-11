c524eb53ac5adc74e911970b39ed8730a211e8f5
achensetyawati <achenzetya@gmail.com>
2023-07-11 15:30:48 +0700

    Update EntityRegistrar.cs

--- src/Manufactures.Data.EntityFrameworkCore.SqlServer/EntityRegistrar.cs
+++ src/Manufactures.Data.EntityFrameworkCore.SqlServer/EntityRegistrar.cs
@@ -459,6 +459,31 @@ namespace Manufactures.Data.EntityFrameworkCore
                 etb.ApplyAuditTrail();
                 etb.ApplySoftDelete();
             });
+<<<<<<< Updated upstream
+=======
+            
+            modelBuilder.Entity<DailyOperationMachineReachingReadModel>(etb =>
+            {
+                etb.ToTable("WeavingDailyOperationReachingMachines");
+                etb.HasKey(e => e.Identity);
+
+                etb.Property(p => p.Group).HasMaxLength(100);
+                etb.Property(p => p.Code).HasMaxLength(100);
+                etb.Property(p => p.BeamNo).HasMaxLength(100);
+
+                etb.ApplyAuditTrail();
+                etb.ApplySoftDelete();
+            });
+
+            modelBuilder.Entity<DailyOperationLoomMachineReadModel>(etb =>
+            {
+                etb.ToTable("WeavingDailyOperationLoomMachines");
+                etb.HasKey(e => e.Identity);
+
+                etb.ApplyAuditTrail();
+                etb.ApplySoftDelete();
+            });
+>>>>>>> Stashed changes
         }
     }
 }
