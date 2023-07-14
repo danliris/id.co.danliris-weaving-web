ced88606d449f7774bba1f31ecdd382e2961fa53
achensetyawati <achenzetya@gmail.com>
2023-07-14 07:50:29 +0700

    Update ReachingDailyOperationReportXlsTemplate.cs

--- src/Manufactures/Helpers/XlsTemplates/ReachingDailyOperationReportXlsTemplate.cs
+++ src/Manufactures/Helpers/XlsTemplates/ReachingDailyOperationReportXlsTemplate.cs
@@ -18,18 +18,15 @@ namespace Manufactures.Helpers.XlsTemplates
 
             reportDataTable.Columns.Add(new DataColumn() { ColumnName = "No", DataType = typeof(int) });
             reportDataTable.Columns.Add(new DataColumn() { ColumnName = "Tanggal", DataType = typeof(string) });
-            reportDataTable.Columns.Add(new DataColumn() { ColumnName = "Nama Operator", DataType = typeof(string) });
-            reportDataTable.Columns.Add(new DataColumn() { ColumnName = "EFF", DataType = typeof(string) });
+            reportDataTable.Columns.Add(new DataColumn() { ColumnName = "Code", DataType = typeof(string) });
             reportDataTable.Columns.Add(new DataColumn() { ColumnName = "Jumlah Beam", DataType = typeof(double) });
             int idx = 1;
             double sumBeam = 0;
             foreach (var item in machineDtos)
             {
-                var eff =Math.Round(Convert.ToDouble(item.Efficiency) * 100, 2);
-                var efficiency = eff.ToString() + "%";
                 var dateFormat = "dd MMM yyyy";
                 var date = item.Periode.ToString(dateFormat);
-                reportDataTable.Rows.Add(idx, date, item.Operator, efficiency, item.BeamNo);
+                reportDataTable.Rows.Add(idx, date, item.Code, item.BeamNo);
                 sumBeam += Convert.ToDouble( item.BeamNo);
                 idx++;
             };
@@ -42,34 +39,34 @@ namespace Manufactures.Helpers.XlsTemplates
                 worksheet.Cells["A2"].Value = "TANGGAL AWAL : " + fromDate.ToShortDateString() + "  TANGGAL AKHIR : " + toDate.ToShortDateString();
                 worksheet.Cells["A3"].Value = "SHIFT : " + shift;
                 worksheet.Cells["A4"].Value = "NO MC : " + mcNo;
-                worksheet.Cells["A" + 1 + ":E" + 1 + ""].Merge = true;
-                worksheet.Cells["A" + 2 + ":E" + 2 + ""].Merge = true;
-                worksheet.Cells["A" + 3 + ":E" + 3 + ""].Merge = true;
-                worksheet.Cells["A" + 4 + ":E" + 4 + ""].Merge = true;
-                worksheet.Cells["A" + 5 + ":E" + 5 + ""].Merge = true;
-                worksheet.Cells["A" + 1 + ":E" + 7 + ""].Style.Font.Bold = true;
-                worksheet.Cells["A6" + ":A7"].Merge = true;
-                worksheet.Cells["B6" + ":B7"].Merge = true;
-                worksheet.Cells["C6" + ":D6"].Merge = true;
+                worksheet.Cells["A" + 1 + ":D" + 1 + ""].Merge = true;
+                worksheet.Cells["A" + 2 + ":D" + 2 + ""].Merge = true;
+                worksheet.Cells["A" + 3 + ":D" + 3 + ""].Merge = true;
+                worksheet.Cells["A" + 4 + ":D" + 4 + ""].Merge = true;
+                worksheet.Cells["A" + 5 + ":D" + 5 + ""].Merge = true;
+                worksheet.Cells["A" + 1 + ":D" + 7 + ""].Style.Font.Bold = true;
+                //worksheet.Cells["A6" + ":A7"].Merge = true;
+                //worksheet.Cells["B6" + ":B7"].Merge = true;
+                //worksheet.Cells["C6" + ":D6"].Merge = true;
 
-                worksheet.Cells["A6"].Value = "No";
-                worksheet.Cells["B6"].Value = "Tanggal";
-                worksheet.Cells["C6"].Value = "EFISIENSI REACHING PER OPERATOR";
-                worksheet.Cells["E6"].Value = "MONITORING PRODUKSI REACHING";
+                //worksheet.Cells["A6"].Value = "No";
+                //worksheet.Cells["B6"].Value = "Tanggal";
+                //worksheet.Cells["C6"].Value = "EFISIENSI REACHING PER OPERATOR";
+                //worksheet.Cells["E6"].Value = "MONITORING PRODUKSI REACHING";
                 worksheet.Cells["A7"].LoadFromDataTable(reportDataTable, true);
 
                 worksheet.Cells["A" + (idx + 7)].Value = "TOTAL BEAM";
-                worksheet.Cells["E" + (idx + 7)].Value = sumBeam;
+                worksheet.Cells["D" + (idx + 7)].Value = sumBeam;
                 worksheet.Cells["A" + (idx + 7) + ":D" + (idx + 7) + ""].Merge = true;
                 worksheet.Cells["A" + (idx + 7)].Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
-                worksheet.Cells["A" + (idx + 7) + ":E" + (idx + 7) + ""].Style.Font.Bold = true;
+                worksheet.Cells["A" + (idx + 7) + ":D" + (idx + 7) + ""].Style.Font.Bold = true;
 
-                worksheet.Cells["A" + 6 + ":E" + (idx + 7) + ""].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
-                worksheet.Cells["A" + 6 + ":E" + (idx + 7) + ""].Style.Border.Top.Style = ExcelBorderStyle.Thin;
-                worksheet.Cells["A" + 6 + ":E" + (idx + 7) + ""].Style.Border.Left.Style = ExcelBorderStyle.Thin;
-                worksheet.Cells["A" + 6 + ":E" + (idx + 7) + ""].Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                worksheet.Cells["A" + 7 + ":D" + (idx + 7) + ""].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                worksheet.Cells["A" + 7 + ":D" + (idx + 7) + ""].Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                worksheet.Cells["A" + 7 + ":D" + (idx + 7) + ""].Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                worksheet.Cells["A" + 7 + ":D" + (idx + 7) + ""].Style.Border.Right.Style = ExcelBorderStyle.Thin;
 
-                worksheet.Cells["A" + 1 + ":E" + (idx + 7) + ""].AutoFitColumns();
+                worksheet.Cells["A" + 1 + ":D" + (idx + 7) + ""].AutoFitColumns();
 
 
                 var stream = new MemoryStream();
