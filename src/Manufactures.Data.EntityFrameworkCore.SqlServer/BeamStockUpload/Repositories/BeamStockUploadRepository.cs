c9267fa2fbe63755afe852cce4db22aae7b3a114
achensetyawati <achenzetya@gmail.com>
2023-07-17 11:34:56 +0700

    BeamStock

---
+++ src/Manufactures.Data.EntityFrameworkCore.SqlServer/BeamStockUpload/Repositories/BeamStockUploadRepository.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Data.EntityFrameworkCore;
+using Manufactures.Domain.BeamStockUpload.Entities;
+using Manufactures.Domain.BeamStockUpload.ReadModels;
+using Manufactures.Domain.BeamStockUpload.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manufactures.Data.EntityFrameworkCore.BeamStockUpload.Repositories
+{
+    public class BeamStockUploadRepository : AggregateRepostory<BeamStock, BeamStockReadModel>, IBeamStockRepository
+    {
+        protected override BeamStock Map(BeamStockReadModel readModel)
+        {
+            return new BeamStock(readModel);
+        }
+    }
+}
