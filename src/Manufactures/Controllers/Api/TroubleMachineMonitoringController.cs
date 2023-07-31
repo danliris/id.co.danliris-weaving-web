761175af9a92a109c9bb60998633c10d1698ac26
achensetyawati <achenzetya@gmail.com>
2023-07-31 14:56:55 +0700

    trouble machine add week

--- src/Manufactures/Controllers/Api/TroubleMachineMonitoringController.cs
+++ src/Manufactures/Controllers/Api/TroubleMachineMonitoringController.cs
@@ -213,11 +213,13 @@ namespace Manufactures.Controllers.Api
             return Ok(result, info: new { page, size, total });
         }
         [HttpGet("monthYear")]
-        public async Task<IActionResult> GetDataByFilter(string month, string yearPeriode)
+        public async Task<IActionResult> GetDataByFilter(int page = 1, int size = 100, string month="0", string yearPeriode="")
         {
             var weavingDailyOperations = _losesQuery.GetDataByFilter(month, yearPeriode);
 
-            return Ok(weavingDailyOperations);
+            var total = weavingDailyOperations.Count();
+            var result = weavingDailyOperations.Skip((page - 1) * size).Take(size);
+            return Ok(result, info: new { page, size, total });
         }
     }
    
