d4c5cce9a5445c5290bb0978cd1763b33c3ad471
achensetyawati <achenzetya@gmail.com>
2023-07-31 14:56:48 +0700

    trouble machine migration add week

--- src/WebApplication/Migrations/20230731064751_add_week_weavingTroubleMachines.Designer.cs
+++ src/WebApplication/Migrations/20230731064751_add_week_weavingTroubleMachines.Designer.cs
@@ -4,14 +4,16 @@ using DanLiris.Admin.Web;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DanLiris.Admin.Web.Migrations
 {
     [DbContext(typeof(AppStorageContext))]
-    partial class AppStorageContextModelSnapshot : ModelSnapshot
+    [Migration("20230731064751_add_week_weavingTroubleMachines")]
+    partial class add_week_weavingTroubleMachines
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
